ac16928cf5b7706e405bcd131a3111011fa78fe5
MeDoeD です <medoed21105@gmail.com>
2024-01-07 10:29:40 +0300

    added code

--- script.py
+++ script.py
@@ -1,2 +1,11 @@
 for i in range(10):
     print(i)
+
+
+
+baka = [1,2,3,4,5,6,7,8,9,10, "11","12","dsfa"]
+for i in baka:
+    if i.isdigit():
+        print(i, "is digit") 
+    else:
+        print(i, "not is digit")
